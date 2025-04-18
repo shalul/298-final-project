e8b5d1ef1634ea6737626211f880ef51e1bac718
margwoz <162177723+margwoz@users.noreply.github.com>
2025-04-18 16:01:56 -0400

    Update school_data_processing.py
    took out unused code

--- school_data_processing.py
+++ school_data_processing.py
@@ -21,18 +21,6 @@ tl_2020_us_zcta510.shx
 Note that only two of these are called explicitly, but those calls call the other files implicitly, so all files are needed
 '''
 
-#class to read in public school information
-class PublicSchool:
-    def __init__(self, shapefile_path):
-        self.schools = gpd.read_file(shapefile_path) #read in gpd file
-        self._standardize_crs() #standardize lat/long format, for compatibility with the census formatting
-
-    def _standardize_crs(self):
-        # Ensure CRS is in WGS84 (lat/lon)
-        if self.schools.crs and not self.schools.crs.is_geographic:
-            self.schools = self.schools.to_crs(epsg=4326)
-
-
 shapefile_path = "SchoolPoints_APS_2024_08_28.shp" #might need to be changed depending on local path
 
 zcta = gpd.read_file("tl_2020_us_zcta510.shp") #load census data as geographic data
