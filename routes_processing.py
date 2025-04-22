1e0a0a349556bebaeda5e8f337b1fe79a44b4720
margwoz <162177723+margwoz@users.noreply.github.com>
2025-04-21 22:17:49 -0400

    Update routes_processing.py

--- routes_processing.py
+++ routes_processing.py
@@ -1,6 +1,25 @@
 import geopandas as gpd
 import pandas as pd
 
+'''
+FILES NEEDED TO RUN THIS CODE:
+Route files:
+MTA Bus Timepoints_20250414.geojson
+
+Census files:
+tl_2020_us_zcta510.cpg
+tl_2020_us_zcta510.dbf
+tl_2020_us_zcta510.prj
+tl_2020_us_zcta510.shp
+tl_2020_us_zcta510.shx
+
+Dataset links:
+https://catalog.data.gov/dataset/tiger-line-shapefile-2020-nation-u-s-2010-5-digit-zip-code-tabulation-areas-zcta5 
+https://www.mta.info/open-data 
+
+Note that only two of these are called explicitly, but those calls call the other files implicitly, so all files are needed
+'''
+
 def extract_route_data_and_overlay_with_census(geojson_path, census_shp_path, output_csv):
 
     routes = gpd.read_file(geojson_path)
